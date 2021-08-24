a661104051d7be18346b8e2ea6f1129b1557d567
pawarshubh57@gmail.com <pawarshubh57@gmail.com>
2021-08-24 23:14:39 +0530

    Routing changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,15 +7,31 @@ import { PropertyListComponent } from './property/property-list/property-list.co
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HousingService } from './services/housing.service';
+import {Routes, RouterModule} from '@angular/router';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
+const appRoutes : Routes = [
+  {path: "", component: PropertyListComponent},
+  {path:"rent-property", component: PropertyListComponent},
+  {path:"add-property", component: AddPropertyComponent},
+  {path:"property-detail/:id", component: PropertyDetailComponent}
+
+]
 @NgModule({
   declarations: [
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
     NavBarComponent,
+    AddPropertyComponent,
+    PropertyDetailComponent
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes), // forRoot() method creates an NgModule that contains all the directives, the given routes, and the Router service itself.
+  ],
   providers: [HousingService],
   bootstrap: [AppComponent],
 })
